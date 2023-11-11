8543252a1752663a66c77a7840645c2432d6a3a8
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-11-11 20:59:29 +0530

    new

--- frontend/src/pages/Books/BookManagement/BookManagement.js
+++ frontend/src/pages/Books/BookManagement/BookManagement.js
@@ -15,27 +15,27 @@ function BookManagement() {
                     <h6><b>Book Management</b></h6>
                     <div className="bookManagementTileRow">
                         <div className="bookManagementTile">
-                            <div className="textContainorBook"> <Link to="/bookBorrow" className="link"><img src="https://cdn1.iconfinder.com/data/icons/borrow-book-filled-outline/340/education_book_read_knowledge_library_study_cover-512.png" /><br></br>Borrow Books</Link></div>
+                            <div className="textContainorBook"> <Link to="/bookBorrow" className="link"><img src="https://cdn1.iconfinder.com/data/icons/borrow-book-glyph/340/book_education_paper_page_read_knowledge_notebook-512.png" /><br></br>Borrow Books</Link></div>
                         </div>
                         <div className="bookManagementTile">
-                            <div className="textContainorBook"><Link to="/bookReturn" className="link"><img src="https://cdn0.iconfinder.com/data/icons/book-store-5/68/41-512.png" /><br></br>Return Books</Link></div>
+                            <div className="textContainorBook"><Link to="/bookReturn" className="link"><img src="https://cdn1.iconfinder.com/data/icons/book-store-8/68/41-512.png" /><br></br>Return Books</Link></div>
                         </div>
                         <div className="bookManagementTile">
                             <div className="textContainorBook"><Link to="/bookPurchaiseAdd" className="link"><img src="https://cdn1.iconfinder.com/data/icons/sale-closing-techniques/64/guidebook-sale-guide-book-marketing-512.png" /><br></br>Book Purchaising</Link></div>
                         </div>
                         <div className="bookManagementTile">
-                            <div className="textContainorBook"><Link to="/bookGovernmentGrants" className="link"><img src="https://cdn2.iconfinder.com/data/icons/business-and-finance-59/200/vector_547_18-512.png" /><br></br>Government Grants</Link></div>
+                            <div className="textContainorBook"><Link to="/bookGovernmentGrants" className="link"><img src="https://cdn4.iconfinder.com/data/icons/book-genres-2/340/book_politics_political_paper_law_government_book-512.png" /><br></br>Government Grants</Link></div>
                         </div>
                     </div>
                     <div className="bookManagementTileRow">
                         <div className="bookManagementTile">
-                            <div className="textContainorBook"><Link to="/bookRemoveBooks" className="link"><img src="https://cdn3.iconfinder.com/data/icons/education-volume-4-1/48/154-512.png" /><br></br>Remove Books</Link></div>
+                            <div className="textContainorBook"><Link to="/bookRemoveBooks" className="link"><img src="https://cdn2.iconfinder.com/data/icons/picol-vector/32/book_sans_remove-512.png" /><br></br>Remove Books</Link></div>
                         </div>
                         <div className="bookManagementTile">
-                            <div className="textContainorBook"><Link to="/bookReplacement" className="link"><img src="https://cdn0.iconfinder.com/data/icons/chemistry-2/65/_Studies-512.png" /><br></br>Book Replacement</Link></div>
+                            <div className="textContainorBook"><Link to="/bookReplacement" className="link"><img src="https://cdn4.iconfinder.com/data/icons/business-vol-1-13/100/Artboard_3-512.png" /><br></br>Book Replacement</Link></div>
                         </div>
                         <div className="bookManagementTile">
-                            <div className="textContainorBook"><Link to="/bookDonation" className="link"><img src="https://cdn4.iconfinder.com/data/icons/charity-86/512/book_donation-knowledge-donate-help-charity-512.png" /><br></br>Book Donation</Link></div>
+                            <div className="textContainorBook"><Link to="/bookDonation" className="link"><img src="https://cdn4.iconfinder.com/data/icons/charity-84/512/book_donation-knowledge-donate-help-charity-512.png" /><br></br>Book Donation</Link></div>
                         </div>
                         <div className="bookManagementTile">
                             <div className="textContainorBook"><Link to="/booksAdd" className="link"><img src="https://cdn2.iconfinder.com/data/icons/boxicons-solid-vol-1/24/bxs-book-add-512.png" /><br></br>Add Books</Link></div>
